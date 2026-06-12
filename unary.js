df47e3525964fff50d2b8b7915b3c077cef262cc
Ramya39rosie <rosieramya556@gmail.com>
2026-06-12 13:50:35 +0530

    added unary.js

---
+++ unary.js
@@ -0,0 +1,11 @@
+var a = 30;
+console.log(a++); // post increament
+// add one number
+var b = 25;
+console.log(b--); // post decreament
+// same num as well as var
+var c = 12;
+console.log(--c);// pre decreament
+var d = 20;
+console.log(++d); // pre increament
+// d value 1st print then add one
--- variables.js
+++
@@ -1,8 +0,0 @@
-var name = "sangavi"
-console.log(name)
-{
-    let a = 10
-    console.log(a)
-}
-const college = "VETIAS"
-console.log(college)
